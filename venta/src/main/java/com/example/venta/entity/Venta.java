672e99d1d6e7e58e1e9bb5787de0caf8055c91c3
jhamil545 <jhamil.apaza@upeu.edu.pe>
2023-06-12 13:06:59 -0500

    asdasd

---
+++ venta/src/main/java/com/example/venta/entity/Venta.java
@@ -0,0 +1,27 @@
+package com.example.venta.entity;
+
+import com.example.venta.dto.Cliente;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.List;
+
+@Entity
+@Data
+
+public class Venta {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String serie;
+    private String numero;
+    private String descripcion;
+    private Integer clienteId;
+    @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "venta_id")
+    private List<VentaDetalle> detalle;
+    @Transient
+    private Cliente cliente;
+}
---
+++ venta/src/main/java/com/example/venta/entity/VentaDetalle.java
@@ -0,0 +1,23 @@
+package com.example.venta.entity;
+
+import com.example.venta.dto.Cliente;
+import com.example.venta.dto.Producto;
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+public class VentaDetalle {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private Double cantidad;
+    private Double precio;
+    private Integer productoId;
+    @Transient
+    private Producto producto;
+    public VentaDetalle() {
+        this.cantidad = (double) 0;
+        this.precio = (double) 0;
+    }
+}
